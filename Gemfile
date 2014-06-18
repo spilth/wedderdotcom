78815f1adb64af6145eeacc5b09c146d7a8618ae
Mark McDonald <pair+mmcdonald@pivotallabs.com>
2014-06-17 20:04:14 -0400

    Initial commit

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
-gem "sinatra"
+gem 'sinatra'
+gem 'image_suckr'
+gem 'slim'
+gem 'rerun'
+gem 'ffaker'
+
--- go.rb
+++ go.rb
@@ -1,5 +1,57 @@
 require 'sinatra'
+require 'slim'
+require 'image_suckr'
+require 'ffaker'
+
+module Faker
+  BASE_LIB_PATH = "."
+
+  module Headline
+    extend ModuleUtils
+    extend self
+
+    def past_tense_verb
+      PAST_TENSE_VERB.rand
+    end
+
+    def pronoun
+      PRONOUN.rand
+    end
+
+    def noun
+      NOUN.rand
+    end
+
+    def verb
+      VERB.rand
+    end
+
+    def number
+      NUMBER.rand
+    end
+  end
+end
 
 get '/' do
-  'Hello world!'
+
+  terms = ["magic", "moon", "bugs", "flowers", "tornado", "child", "death"]
+  term = terms.sample
+  past_tense_verb = Faker::Headline.past_tense_verb
+  pronoun = Faker::Headline.pronoun
+  number = Faker::Headline.number
+  noun = Faker::Headline.noun
+  verb = Faker::Headline.verb
+
+  sentences = [
+    "#{pronoun.capitalize} #{past_tense_verb} WHAT in WHERE?",
+    "#{number} #{noun}s that will #{verb.upcase} your mind",
+    "#{pronoun.capitalize} can #{verb} with THIS #{noun}?"
+  ]
+  headline = sentences.sample
+  suckr = ImageSuckr::GoogleSuckr.new
+  slim :index, locals: {
+    suckr: suckr,
+    term: term,
+    headline: headline
+  }
 end
